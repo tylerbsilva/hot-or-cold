ea82239f5471f83011610690784b8190118c72f3
Tyler Silva <tylerbsilva@gmail.com>
2015-05-21 08:47:39 -0500

    Beautify-d file

--- js/app.js
+++ js/app.js
@@ -1,17 +1,14 @@
+$(document).ready(function() {
 
-$(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+  /*--- Display information modal box ---*/
+  $(".what").click(function() {
+    $(".overlay").fadeIn(1000);
 
-  	});
+  });
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+  /*--- Hide information modal box ---*/
+  $("a.close").click(function() {
+    $(".overlay").fadeOut(1000);
+  });
 
 });
-
-
