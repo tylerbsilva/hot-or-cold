c65dbcd750ea810e5220f6a5b3d1919e90b220f2
Tyler Silva <tylerbsilva@gmail.com>
2015-05-21 12:09:25 -0500

    Started to add JS to make the app work!

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+var randomNumber = Math.floor((Math.random() * 100) + 1);
+var guesses = 0;
+
 $(document).ready(function() {
 
   /*--- Display information modal box ---*/
@@ -11,4 +14,43 @@ $(document).ready(function() {
     $(".overlay").fadeOut(1000);
   });
 
+	// click on new game - reset
+	$(".new").click(function(){
+		randomNumber = Math.floor((Math.random() * 100) + 1);
+		guesses = 0;
+		$("#count").text(guesses);
+		$(".guessForm").show();
+		$("#feedback").text("Make your Guess!");
+		$('#guessList').text('');
+	});
+
+	// guess number
+	$(document).on('click', '#guessButton', function() {
+		// check if it's close
+		var guess = parseInt($("input[name='userGuess']").val().trim());
+		if (guess) {
+			guesses+=1;
+			$("#count").text(guesses);
+			$('#userGuess').val("");
+			if (guess == randomNumber) {
+				$("#guessList").append("<li class='correct'>" + guess + "</li>");
+				$(".guessForm").hide();
+				$("#feedback").text("NICE JOB!");
+			}	else if (guess > randomNumber) {
+				$("#guessList").append("<li>" + guess + "</li>");
+				$("#feedback").text("Try Again");
+			} else if (guess < randomNumber) {
+				$("#guessList").append("<li>" + guess + "</li>");
+				$("#feedback").text("Try Again");
+			} else {
+			}
+		}
+	});
+
+	// Prevent default submission
+	$('.guessForm').submit( function(e) {
+				e.preventDefault();
+
+				return false;
+		});
 });
