ecb53a7e97044e16779eed8a6c07d5aa79394b63
Tyler Silva <tylerbsilva@gmail.com>
2015-05-25 16:29:08 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,72 @@
+//Hot and Cold, JS App written by Tyler Silva
+
+/* Global Variables */
+
+
 var randomNumber = getRandom();
 var guesses = 1;
+
+/* Functions */
+
+function getRandom() {
+  return Math.floor((Math.random() * 100) + 1);
+}
+
+// grabs the input from the user and returns input if it is a number between 1-100, else false
+function getGuess(){
+  var input = parseInt($("input[name='userGuess']").val().trim());
+  guesses += 1;
+  if (guess <= 100 && guess > 0){
+    return input;
+  } else {
+    return false;
+  }
+}
+
+// checks if num matches randomNumber
+function correct(num){
+  if (randomNumber == num) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// checks if num is within 5 of randomNumber
+function within5(num){
+  if (randomNumber + 5 >= num && randomNumber - 5 <= num) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// checks if num is within 10 of randomNumber
+function within10(num){
+  if (randomNumber + 10 >= num && randomNumber - 10 <= num) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// checks if num is within 20 of randomNumber
+function within20(num){
+  if (randomNumber + 20 >= num && randomNumber - 20 <= num) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// checks if num is within 30 of randomNumber
+function within30(num){
+  if (randomNumber + 30 >= num && randomNumber - 30 <= num) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 $(document).ready(function() {
 
@@ -14,63 +81,57 @@ $(document).ready(function() {
     $(".overlay").fadeOut(1000);
   });
 
-	// click on new game - reset
-	$(".new").click(function(){
-		randomNumber = getRandom();
-		guesses = 1;
-		$("#count").text(guesses);
-		$(".guessForm").show();
-		$("#feedback").text("Make your Guess!");
-		$('#guessList').text('');
-	});
+  // click on new game - reset
+  $(".new").click(function(){
+    randomNumber = Math.floor((Math.random() * 100) + 1);
+    guesses = 1;
+    $("#count").text(guesses);
+    $(".guessForm").show();
+    $("#feedback").text("Make your Guess!");
+    $('#guessList').text('');
+  });
 
-	// guess number
-	$('#guessButton').click(function() {
-		// get input and check if number
-		var guess = parseInt($("input[name='userGuess']").val().trim());
-		if (guess <= 100 && guess > 0) {
-			// add to count
-			guesses+=1;
-			$("#count").text(guesses);
-			$('#userGuess').val("");
-			// check if correct
-			if (guess == randomNumber) {
-				$("#guessList").append("<li class='correct'>" + guess + "</li>");
-				$(".guessForm").hide();
-				$("#feedback").text("NICE JOB! The number was " + randomNumber);
-			}
-			// check if within 10
-			else if (randomNumber + 10 >= guess && randomNumber - 10 <= guess) {
-				$("#guessList").append("<li>" + guess + "</li>");
-				$("#feedback").text("ON FIRE!!!");
-			}
-			// check if within 25
-			else if (randomNumber + 25 >= guess && randomNumber - 25 <= guess) {
-				$("#guessList").append("<li>" + guess + "</li>");
-				$("#feedback").text("Getting Warmer");
-			}
-			//check if within 50
-			else if (randomNumber + 50 >= guess && randomNumber - 50 <= guess) {
-				$("#guessList").append("<li>" + guess + "</li>");
-				$("#feedback").text("Lukewarm...");
-			}
-			// Check if over 50 away...
-			else {
-				$("#guessList").append("<li>" + guess + "</li>");
-				$("#feedback").text("COLD AS ICE.");
-			}
-		}
-	});
-
-	// Prevent default submission
-	$('.guessForm').submit( function(e) {
-				e.preventDefault();
-
-				return false;
-		});
+  // guess number
+  $('.guessForm').submit(function(e) {
+    //prevent default submission
+    e.preventDefault();
+    // get input and check if number
+    var guess = getGuess();
+    $("#count").text(guesses);
+    $('#userGuess').val("");
+    // if input is a number, check the conditions
+    if (guess) {
+      // checks if the guess is correct
+      if (correct(guess)) {
+        $("#guessList").append("<li class='correct'>" + guess + "</li>");
+        $(".guessForm").hide();
+        $("#feedback").text("NICE JOB! The number was " + randomNumber);
+      }
+      // check if within 5
+      else if (within5(guess)) {
+        $("#guessList").append("<li>" + guess + "</li>");
+        $("#feedback").text("ON FIRE!!!");
+      }
+      // check if within 10
+      else if (within10(guess)) {
+        $("#guessList").append("<li>" + guess + "</li>");
+        $("#feedback").text("Hotter");
+      }
+      //check if within 20
+      else if (within20(guess)) {
+        $("#guessList").append("<li>" + guess + "</li>");
+        $("#feedback").text("Warmer");
+      }
+      //check if within 30
+      else if (within30(guess)) {
+        $("#guessList").append("<li>" + guess + "</li>");
+        $("#feedback").text("Lukewarm...");
+      }
+      // Default if doesn't match any conditions
+      else {
+        $("#guessList").append("<li>" + guess + "</li>");
+        $("#feedback").text("COLD AS ICE.");
+      }
+    }
+  });
 });
-
-function getRandom() {
-  var num = Math.floor((Math.random() * 100) + 1);
-  return num;
-}
